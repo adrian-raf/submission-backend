ab4c9bb39b54bd44bc505421c500ad3495e16294
Adrian Rafly <adrianrafly.ar@gmail.com>
2022-02-12 08:51:22 +0700

    submission backend Bookshelf API

---
+++ src/routes.js
@@ -0,0 +1,41 @@
+const {
+  addBooksHandler,
+  getAllBooksHandler,
+  getSpecifiedBookHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getSpecifiedBookHandler,
+  },
+
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+];
+
+module.exports = routes;
